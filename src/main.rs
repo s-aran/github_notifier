f743b0a2ed63a009f8d11d9154fb24df7ef0373a
s.aran <s-aran@users.noreply.github.com>
2024-03-11 01:23:33 +0900

    show all abandoned pulls

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,6 @@ fn main() {
 
     let protected_branches = match rt.block_on(async {
         let github = octocrab::instance();
-    
         github
             .repos(&settings.github.user, &settings.github.repository)
             .list_branches()
@@ -53,7 +52,6 @@ fn main() {
     // println!("{:#?}", protected_branches);
     let page = match rt.block_on(async {
         let github = octocrab::instance();
-    
         github
             .pulls(&settings.github.user, &settings.github.repository)
             .list()
@@ -75,24 +73,32 @@ fn main() {
         }
     };
 
-    let abandoned_pulls: Vec<&PullRequest> = page.items.iter().filter(|p| {
+    let filtered_abandoned_pulls: Vec<&PullRequest> = page.items.iter().filter(|p| {
         let created_at = match &p.created_at {
             Some(t) => t,
             None => return false,
         };
         let delta = chrono::Utc::now() - created_at;
         let delta_days = delta.num_days();
-        println!("{:?}", &delta_days);
         delta_days > (settings.github.abandoned_days as i64)
     }).collect();
 
-    println!("--------------------------------------------------------------------------------");
-    for pull in abandoned_pulls.iter() {
-        println!("{:?}", pull);
-    }
-    println!("--------------------------------------------------------------------------------");
+    let abandoned_pulls: Vec<PullRequest> = filtered_abandoned_pulls.iter().map(|p| {
+        let github = octocrab::instance();
+        match rt.block_on(async {
+            github.pulls(&settings.github.user, &settings.github.repository)
+            .get(p.number)
+            .await
+            }) {
+                Ok(p) => p.clone(),
+                Err(e) => {
+                    eprintln!("{}", e);
+                    panic!();
+                }
+            }
+        }).collect();
 
-    {
+    for p in &abandoned_pulls{
         // let first_page = match rt.block_on(async {
         // let github = octocrab::instance();
         // github.pulls(&settings.github.user, &settings.github.repository).get(3).await})
@@ -105,20 +111,19 @@ fn main() {
         // };
 
         // let first_page = page.items.get(0).unwrap();
-        let first_page = abandoned_pulls.get(0).unwrap();
          // println!("{:?}", first_page);
-        let pr_num = &first_page.number;
-        let url = &first_page.url;
+        let pr_num = &p.number;
+        let url = &p.url;
         let default_title = "(none)".to_owned();
-        let title = first_page.title.as_ref().unwrap_or(&default_title);
-        let created_at = &first_page.created_at;
-        let updated_at = &first_page.updated_at;
-        let closed_at = &first_page.closed_at;
-        let merged_at = &first_page.merged_at;
+        let title = p.title.as_ref().unwrap_or(&default_title);
+        let created_at = &p.created_at;
+        let updated_at = &p.updated_at;
+        let closed_at = &p.closed_at;
+        let merged_at = &p.merged_at;
         let default_request_reviewers = Vec::new();
-        let requested_reviewers = first_page.requested_reviewers.as_ref().unwrap_or(&default_request_reviewers);
-        let mergeable = &first_page.mergeable;
-        let mergeable_state = &first_page.mergeable_state;
+        let requested_reviewers = p.requested_reviewers.as_ref().unwrap_or(&default_request_reviewers);
+        let mergeable = &p.mergeable;
+        let mergeable_state = &p.mergeable_state;
 
         println!("{:?}", pr_num);
         println!("{:?}", url);
@@ -139,24 +144,18 @@ fn main() {
             println!("{:?}", requested_reviewer.email.as_ref().unwrap_or(&default_email));
         }
 
-        let delta = match TimeDelta::try_days(90) {
-            Some(d) => d,
-            None => {
-                eprintln!("TimeDelta failed.");
-                return;
-            }
-        };
-
         let created = created_at.unwrap();
-        println!("{:?}", created);
-        println!("{:?}", created - delta);
+        // println!("{:?}", created);
+        // println!("{:?}", created - delta);
         let now = chrono::Utc::now();
-        println!("{:?}",now-created);
+        // println!("{:?}",now-created);
         let diff = now -created;
         println!("days: {}", diff.num_days());
+
+        println!("----------------------------------------------------------------------------------------------------");
     }
 
     slack::message::send::post(&settings, "test message".to_owned());
-    slack::message::send::post_with_webhook(&settings, "test message".to_owned());
+    // slack::message::send::post_with_webhook(&settings, "test message".to_owned());
 
 }
